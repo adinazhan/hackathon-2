22a34659e43414fe5e907ff2f5ce129f420570e7
Adina Zhantykeyeva <adinazhantykeyeva@Adinas-MacBook-Air.local>
2024-10-04 10:04:41 +0200

    get api

---
+++ script.js
@@ -0,0 +1,18 @@
+const getData = async () => {
+  const response = await fetch(
+    "https://test-api.codingbootcamp.cz/api/4d66b532/events"
+  );
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+};
+getData();
+
+class eventName {
+  constructor(name, date, description) {
+    this.name = name;
+    this.date = date;
+    this.description = description;
+  }
+  createWidget() {}
+}
