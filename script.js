4f09ee85a63f851a1772756f7ad3c005f64d0543
Adina Zhantykeyeva <adinazhantykeyeva@Adinas-MacBook-Air.local>
2024-10-04 14:52:42 +0200

    styling

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 const getData = async () => {
-  try {
-    const response = await fetch(
-      "https://test-api.codingbootcamp.cz/api/4d66b532/events"
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+  const response = await fetch(
+    "https://test-api.codingbootcamp.cz/api/4d66b532/events"
+  );
+  const data = await response.json();
+  return data;
 };
-
-class eventName {
+class EventName {
   constructor(name, date, description) {
     this.name = name;
     this.date = date;
     this.description = description;
     this.createWidget();
   }
-
   createWidget() {
     const addingInfoToDiv = document.querySelector(".events");
     const docEl = document.createElement("div");
     docEl.classList.add("event__widget");
-
     docEl.innerHTML = `
       <h3 class="name">${this.name}</h3>
       <p class="description">${this.description}</p>
@@ -32,37 +25,39 @@ class eventName {
     addingInfoToDiv.appendChild(docEl);
   }
 }
-
 getData().then((data) => {
   data.forEach((event) => {
-    new eventName(event.name, event.date, event.description);
+    new EventName(event.name, event.date, event.description);
   });
+  buttonRegistration(); // Initialize button events after elements are created
 });
-
 const myModal = () => {
-  const addingModalToDIv = document.querySelector(".events");
-  const docModal = document.createElement("div");
-  docModal.classList.add("my__modal");
-  docModal.innerHTML = `
-  <form action="">
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="lname">Surname:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <label for="lname">Email:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <label for="fname">Phone number:</label>
-  <input type="number" id="fname" name="fname"><br><br>
-  <label for="fname">I am old enough to participate</label>
-  <input type="checkbox" id="fname" name="fname"><br><br>
-  <input type="submit" value="Submit">
-</form>
-  `;
-  addingModalToDIv.appendChild(docModal);
+  const docModal = document.querySelector(".my__modal");
+  const addingModalToDiv = document.querySelector(".events");
+  const newModal = document.createElement("div");
+  newModal.classList.add("my__modal");
+  newModal.innerHTML = `
+      <form action="">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="lname">Surname:</label>
+        <input type="text" id="lname" name="lname"><br><br>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="phone">Phone number:</label>
+        <input type="number" id="phone" name="phone"><br><br>
+        <label for="age">I am old enough to participate</label>
+        <input type="checkbox" id="age" name="age"><br><br>
+        <input type="submit" value="Submit">
+      </form>
+    `;
+  addingModalToDiv.appendChild(newModal);
 };
-myModal();
-
 const buttonRegistration = () => {
-  const myButton = document.querySelector(".registration__button");
-  myButton.addEventListener("click");
+  const buttons = document.querySelectorAll(".registration__button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      myModal();
+    });
+  });
 };
